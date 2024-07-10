0736cd5c264e0b2f00b41656d63668d2e350b46e
Alexandra Leonora Acuña Tomacheski <aletomacheskii@gmail.com>
2024-07-10 09:41:41 -0300

    ddd

--- assets/script.js
+++ assets/script.js
@@ -20,29 +20,7 @@ openMenu.addEventListener ('click', () => {
         menu.removeAttribute=('style')
         openMenu.removeAttribute('style')
     }, 200)
-      // Seleciona elementos do DOM
-      const textoEditavel = document.getElementById('texto-editavel');
-      const botaoSalvar = document.getElementById('botao-salvar');
-      
-  
-      // Evento para habilitar a edição do texto
-      botaoSalvar.addEventListener('click', salvarTexto, () => {
-          textoEditavel.setAttribute('contenteditable', 'true');
-          textoEditavel.focus(); // Coloca o foco no texto editável
-
-          // funçao da aplicação
-          function salvarTexto() {
-            localStorage.setItem('textoSalvo', textoEditavel.innerText);
-            alert('Texto salvo com sucesso!');
-        }
-            // Verifica se há um texto salvo no localStorage e carrega se existir
-            if (localStorage.getItem('textoSalvo')) {
-                textoEditavel.innerText = localStorage.getItem('textoSalvo');
-            }        
-      });
-       // Evento para salvar o texto ao clicar no botão "Salvar"
-      
-
+     
    })
    
     
